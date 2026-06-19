2f7f4e640c4bc3b90aa407a416d2ed1c0db549fb
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-19 23:54:50 +0000

    Array & HashMap - add valid anagram question

---
+++ 0_array_hashing/ValidAnagram.java
@@ -0,0 +1,16 @@
+/**
+ * https://neetcode.io/problems/duplicate-integer/
+ * 
+ * Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+ * 
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ * 
+ * Constraints:
+ * 1 <= s.length, t.length <= 5 * 10^4
+ * s and t consist of lowercase English letters.
+ */
+
+public class ValidAnagram {
+    // public boolean isAnagram(int[] nums) {
+    // }
+}
