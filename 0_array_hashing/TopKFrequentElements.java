85e49e1a3ff165fe23c8fd7eb9b52001b3a75470
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-20 17:17:46 +0000

    Arrays & HashMap - TopKFrequentElements question

---
+++ 0_array_hashing/TopKFrequentElements.java
@@ -0,0 +1,17 @@
+/**
+ * https://neetcode.io/problems/top-k-elements-in-list/question?list=neetcode150
+ * 
+ * Given an integer array nums and an integer k, return the k most frequent elements within the array.
+ * The test cases are generated such that the answer is always unique.
+ * You may return the output in any order.
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 10^4.
+ * -1000 <= nums[i] <= 1000
+ * 1 <= k <= number of distinct elements in nums.
+ */
+public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+    }
+}
