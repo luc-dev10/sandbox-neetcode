901444268537c020678291d8e9007ed54daf254c
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-20 16:29:23 +0000

    Array & Hashmap - TwoSum question

---
+++ 0_array_hashing/TwoSum.java
@@ -0,0 +1,23 @@
+/*
+ * https://neetcode.io/problems/two-integer-sum/question?list=neetcode150
+ *
+ * Given an array of integers nums and an integer 
+ * target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+ *
+ * You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+ * 
+ * Return the answer with the smaller index first.
+ * 
+ * Constraints:
+ * 
+ * 2 <= nums.length <= 1000
+ * -10,000,000 <= nums[i] <= 10,000,000
+ * -10,000,000 <= target <= 10,000,000
+ * Only one valid answer exists.
+ * 
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+    }
+}
