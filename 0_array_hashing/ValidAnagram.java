cfda00c36c87543ca81398092852c329280cd084
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-20 16:25:05 +0000

    Array & HashMap - Valid Anagram solution

--- 0_array_hashing/ValidAnagram.java
+++ 0_array_hashing/ValidAnagram.java
@@ -11,6 +11,19 @@
  */
 
 public class ValidAnagram {
-    // public boolean isAnagram(int[] nums) {
-    // }
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> hm = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!hm.containsKey(c)) return false;
+            if (hm.get(c) == 1) hm.remove(c);
+            else hm.put(c, hm.get(c) - 1);
+        }
+
+        return hm.isEmpty();
+    }
 }
