9593809b935b7ccea4669d8253862390e74f284e
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-20 18:21:07 +0000

    Array & Hashmap - TopKFrequentElements solution

--- 0_array_hashing/TopKFrequentElements.java
+++ 0_array_hashing/TopKFrequentElements.java
@@ -12,6 +12,27 @@
  */
 public class TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        List<Integer>[] f = new ArrayList[nums.length + 1];
+
+        for (int n : nums) hm.put(n, hm.getOrDefault(n ,0) + 1);
         
+        for (Map.Entry<Integer, Integer> e : hm.entrySet()) {
+            if (f[e.getValue()] == null) f[e.getValue()] = new ArrayList<>();
+            f[e.getValue()].add(e.getKey());
+        }
+        
+        int[] res = new int[k];
+        int t = 0;
+        for (int i = f.length - 1; i >= 0 && t < k; i--) {
+            List<Integer> l = f[i];
+            if (l == null) continue;
+
+            for (int j = 0; j < l.size() && t < k; j++) {
+                res[t++] = l.get(j);
+           }
+        }
+
+        return res;
     }
 }
