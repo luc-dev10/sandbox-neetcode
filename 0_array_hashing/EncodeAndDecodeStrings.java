5884897169b9f0d49d73468be576bc4959de87e4
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-20 18:28:06 +0000

    Arrays & HashMap - EncodeAndDecodeStrings question

---
+++ 0_array_hashing/EncodeAndDecodeStrings.java
@@ -0,0 +1,20 @@
+/**
+ * https://neetcode.io/problems/string-encode-and-decode/question?list=neetcode150
+ * 
+ * Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+ *
+ * Constraints:
+ * 0 <= strs.length < 100
+ * 0 <= strs[i].length < 200
+ * strs[i] contains any possible characters out of 256 valid ASCII characters.
+ * 
+ */
+public class EncodeAndDecodeStrings {
+    public String encode(List<String> strs) {
+
+    }
+
+    public List<String> decode(String str) {
+
+    }
+}
