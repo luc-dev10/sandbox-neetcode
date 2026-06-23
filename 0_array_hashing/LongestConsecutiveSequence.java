e4be0ebe60faa70ec33250d5ec6c666ed0d2161f
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-23 23:43:50 +0000

    Array & HashMap - Longest Consecutive Sequence question

---
+++ 0_array_hashing/LongestConsecutiveSequence.java
@@ -0,0 +1,17 @@
+/**
+ * https://neetcode.io/problems/longest-consecutive-sequence/question?list=neetcode150
+ * 
+ * Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+ * A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
+ * You must write an algorithm that runs in O(n) time.
+ *
+ * Constraints:
+ * 0 <= nums.length <= 1000
+ * -10^9 <= nums[i] <= 10^9
+ * 
+ */
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        
+    }
+}
