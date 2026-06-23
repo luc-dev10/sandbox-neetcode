ae09354eb1f9c1e333f7c47a47ebc2f7eb28932a
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-23 22:33:54 +0000

    Array & HashMap - Products of Array Except Self question

---
+++ 0_array_hashing/ProductsOfArrayExceptSelf.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * https://neetcode.io/problems/products-of-array-discluding-self/question?list=neetcode150
+ * 
+ * Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+ * Each product is guaranteed to fit in a 32-bit integer.
+ * Follow-up: Could you solve it in O(n) & O(n) time without using the division operation?
+ * 
+ * Constraints:
+ * 2 <= nums.length <= 1000
+ * -20 <= nums[i] <= 20
+ * 
+*/
+public class ProductsOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+
+    }
+}
