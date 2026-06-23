a70b525f3cd54f43d6c9dbacf2a4a7acccbbb1b9
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-23 23:31:41 +0000

    Arrays & HashMap - Valid Sudoku solution

--- 0_array_hashing/ValidSudoku.java
+++ 0_array_hashing/ValidSudoku.java
@@ -15,5 +15,27 @@
  */
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> r = new HashMap<>();
+        Map<Integer, Set<Character>> c = new HashMap<>();
+        Map<String, Set<Character>> s = new HashMap<>();
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                String sk = i/3 + "_" + j/3;
+                
+                if (r.computeIfAbsent(i, k -> new HashSet<>()).contains(board[i][j]) ||
+                    c.computeIfAbsent(j, k -> new HashSet<>()).contains(board[i][j]) ||
+                    s.computeIfAbsent(sk, k -> new HashSet<>()).contains(board[i][j])) {
+                    return false;
+                }
+
+                r.get(i).add(board[i][j]);
+                c.get(j).add(board[i][j]);
+                s.get(sk).add(board[i][j]);
+            }
+        }
+
+        return true;
     }
 }
