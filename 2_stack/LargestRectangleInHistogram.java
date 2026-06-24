41721145381969e8dd8f27d3679463ded521c0e2
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-24 00:01:08 +0000

    Stack - Largest Rectangle In Histogram question

---
+++ 2_stack/LargestRectangleInHistogram.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * https://neetcode.io/problems/largest-rectangle-in-histogram/question?list=neetcode150
+ * 
+ * You are given an array of integers heights where heights[i] represents the height of a bar. The width of each bar is 1.
+ * Return the area of the largest rectangle that can be formed among the bars.
+ * Note: This chart is known as a histogram.
+ * 
+ * Constraints:
+ * 1 <= heights.length <= 1000.
+ * 0 <= heights[i] <= 1000
+ */
+public class LargestRectangleInHistogram {
+    public int largestRectangleArea(int[] heights) {
+    }
+}
