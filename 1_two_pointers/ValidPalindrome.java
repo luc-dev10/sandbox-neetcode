566a9602ede638ae6d2a52dda3966263e6697fb5
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-25 22:45:40 +0000

    Two Pointers - Valid Palindrome question

---
+++ 1_two_pointers/ValidPalindrome.java
@@ -0,0 +1,16 @@
+
+/**
+ * https://neetcode.io/problems/is-palindrome/question?list=neetcode150
+ * 
+ * Given a string s, return true if it is a palindrome, otherwise return false.
+ * A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+ * Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+ * 
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s is made up of only printable ASCII characters.
+ * 
+ */
+public class ValidPalindrome {
+
+}
