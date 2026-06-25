dc084f2552aaa7cfe4409aacafbbbf1fb008c1fc
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-25 23:04:34 +0000

    Two Pointers - Three Sum question

---
+++ 1_two_pointers/ThreeSum.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * https://neetcode.io/problems/three-integer-sum/question?list=neetcode150
+ * 
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+ * The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+ * 
+ * Constraints:
+ * 3 <= nums.length <= 1000
+ * -10^5 <= nums[i] <= 10^5
+ * 
+ */
+public class TreeSum {
+    
+}
