7a37c77f7e08e3c8dd8fc47413b276a7b2f820b1
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-25 23:57:10 +0000

    Two Pointers - Three Sum solution

--- 1_two_pointers/ThreeSum.java
+++ 1_two_pointers/ThreeSum.java
@@ -11,5 +11,28 @@
  * 
  */
 public class TreeSum {
-    
+        public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i - 1] == nums[i]) continue;
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+                int t = nums[l] + nums[r] + nums[i];
+                if (t == 0) {
+                    res.add(List.of(nums[i], nums[l++], nums[r--]));
+                    while (l < nums.length && nums[l - 1] == nums[l]) l++;
+                } else if (t < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return res;
+    }
 }
