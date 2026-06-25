a9230ade7065887d30683933e50c66606aaba236
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-25 22:57:13 +0000

    Two Pointers - Two Integer Sum II questions

---
+++ 1_two_pointers/TwoIntegerSumII.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * https://neetcode.io/problems/two-integer-sum-ii/question?list=neetcode150
+ * 
+ * Given an array of integers numbers that is sorted in non-decreasing order.
+ * Return the indices (1-indexed) of two numbers, [index1, index2], such that they add up to a given target number target and index1 < index2. Note that index1 and index2 cannot be equal, therefore you may not use the same element twice.
+ * There will always be exactly one valid solution.
+ * Your solution must use O(1) O(1) additional space.
+ * 
+ * Constraints:
+ * 2 <= numbers.length <= 1000
+ * -1000 <= numbers[i] <= 1000
+ * -1000 <= target <= 1000
+ */
+public class TwoIntegerSumII {
+
+}
