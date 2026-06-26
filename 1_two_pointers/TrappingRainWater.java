00db050d325cd6aaab15642559c034e1ccdbd29e
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-26 00:09:57 +0000

    Two Pointers - Trapping Rain Water question

---
+++ 1_two_pointers/TrappingRainWater.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * https://neetcode.io/problems/trapping-rain-water/question?list=neetcode150
+ * 
+ * You are given an array of non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
+ * Return the maximum area of water that can be trapped between the bars.
+ *
+ * Constraints:
+ * 1 <= height.length <= 1000
+ * 0 <= height[i] <= 1000
+ */
+public class TrappingRainWater {
+    
+}
