c5c0eed0cb8d7fb307d87fc49a3c723ddf7b63d0
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-26 00:01:10 +0000

    Two Pointers - Container With Most Water question

---
+++ 1_two_pointers/ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ * https://neetcode.io/problems/max-water-container/question?list=neetcode150
+ * 
+ * You are given an integer array heights where heights[i] represents the height of the ith bar.
+ * You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+ * 
+ * Constraints:
+ * 2 <= height.length <= 1000
+ * 0 <= height[i] <= 1000
+ */
+public class ContainerWithMostWater {
+    
+}
