cf5fb8aa38815b07ca529e0a435a5ef20af0aa2e
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-26 00:07:43 +0000

    Two Pointers - Container With Most Water solution

--- 1_two_pointers/ContainerWithMostWater.java
+++ 1_two_pointers/ContainerWithMostWater.java
@@ -11,5 +11,19 @@
  * 0 <= height[i] <= 1000
  */
 public class ContainerWithMostWater {
-    
+    public int maxArea(int[] heights) {
+        int l = 0;
+        int r = heights.length - 1;
+        int m = 0;
+
+        while (l < r) {
+            int w = r - l;
+            int h = Math.min(heights[l], heights[r]);
+            m = Math.max(w * h, m);
+            if (heights[l] < heights[r]) l++;
+            else r--; 
+        }
+
+        return m;
+    }
 }
