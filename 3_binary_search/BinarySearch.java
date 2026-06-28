6195d034554fb2d8e94f1099bae8416a4ee36046
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-28 14:35:25 +0000

    Binary Search - Binary Search solution

--- 3_binary_search/BinarySearch.java
+++ 3_binary_search/BinarySearch.java
@@ -13,5 +13,17 @@
  * 
  */
 public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
 
+        while (l <= r) {
+            int m = ((r - l) / 2) + l;
+            if (nums[m] == target) return m;
+            else if (nums[m] < target) l = m + 1;
+            else r = m - 1;
+        }
+
+        return -1;
+    }
 }
