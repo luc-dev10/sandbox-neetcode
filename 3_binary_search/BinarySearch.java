a18a6e652b505a484eb41db85a53844e8e57600c
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-28 14:24:45 +0000

    Binary Search - Binary Search question

---
+++ 3_binary_search/BinarySearch.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * https://neetcode.io/problems/binary-search/question?list=neetcode150
+ * 
+ * You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+ * Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+ * Your solution must run in O(logn) time.
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 10000.
+ * -10000 < nums[i], target < 10000
+ * All the integers in nums are unique.
+ * 
+ */
+public class BinarySearch {
+
+}
