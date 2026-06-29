adaf3521aaefedaaf75feaefbb706321780913c4
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2026-06-29 22:13:27 +0000

    Binary Search - Search a 2D Matrix question

---
+++ 3_binary_search/SearchATwoDMatrix.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * https://neetcode.io/problems/search-2d-matrix/question?list=neetcode150
+ * 
+ * You are given an m x n 2-D integer array matrix and an integer target.
+ * Each row in matrix is sorted in non-decreasing order.
+ * The first integer of every row is greater than the last integer of the previous row.
+ * Return true if target exists within matrix or false otherwise.
+ * Can you write a solution that runs in O(log(m * n)) time?
+ * 
+ * Constraints:
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 100
+ * -10000 <= matrix[i][j], target <= 10000
+ * 
+ */
+
+public class SearchATwoDMatrix {
+    
+}
